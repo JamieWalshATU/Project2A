c4f278531746f73d2d12c111cefe77ba755d93c9
JamieWalshATU <G00433833@ATU.IE>
2024-11-25 17:22:11 +0000

    MAUI Project Template

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Project2A
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
